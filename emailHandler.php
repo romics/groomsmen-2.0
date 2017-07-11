33c94270553f19ce8ba74dd7bdc61a8af0c64d03
Romico Macatula <rmac817@vt.edu>
2017-07-11 11:31:20 -0400

    Added Form Handling capabilities

--- emailHandler.php
+++ emailHandler.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
+if($_POST){
+    $name = $_POST['full-name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
 
-
-
- ?>
+//send email
+    mail("rmac817@vt.edu", "This is an email from:" .$name, 
+    	"Name: " .$name \n \n
+    	"Email: " .$email \n \n
+    	"Phone: " .$phone);
+}
+?>
